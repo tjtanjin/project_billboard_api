8b88dcdf96f263f99a28969f3cfc5b7767b3120d
tjtanjin <cjtanjin@gmail.com>
2019-05-01 17:32:31 +0800

    Test params 6.

--- projectbillboard.py
+++ projectbillboard.py
@@ -10,14 +10,14 @@ def hello():
     return "Test"
 
 class Prediction(Resource):
-    def get(self, songname):
+    def get(self):
         return songname
 
 class haha(Resource):
     def get(self):
         return {"hehe":"hoho"}
 
-api.add_resource(Prediction, '/predict/?songname=<str:songname>/')
+api.add_resource(Prediction, '/predict/?songname=/')
 api.add_resource(haha, '/laugh')
 
 if __name__ == '__main__':
