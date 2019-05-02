b2c3daf761671a8eecded2d38a930f4c0f752192
tjtanjin <cjtanjin@gmail.com>
2019-05-02 18:24:36 +0800

    Fix import render_template.

--- projectbillboard.py
+++ projectbillboard.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Api, Resource
 from flask_jsonpify import jsonify
 from flask_cors import CORS
