e1f8188bc50e36ea3ebb90523bd71f2fbc1b33c0
tjtanjin <cjtanjin@gmail.com>
2019-05-06 23:27:10 +0800

    Updated website looks.

---
+++ templates/js/sp.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  console.log('ready');
+    $("#main_button").click(function(evt) {
+      evt.preventDefault();
+      console.log("main button is clicked");
+      var songName = $('#text_box')[0].value;
+      var model =  document.getElementById("model_selection").value;
+      const data = {chosen_model: model};
+      // get request
+      $.ajax({
+        // put your api endpoint here
+        url: 'https://project-billboard-api.herokuapp.com/predict/'+songName,
+        type: "POST",
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: 'application/json',
+        // function callback after success ajax request
+        success: function(data) {
+          console.log(data)
+
+          document.getElementById("predict_result").style.backgroundColor = "rgb(100,100,100,0.8)"
+          document.getElementById("predict_result").innerHTML = "The probability of "+songName+" being a hit song is: "+data["popularity"];
+          $('#text_box')[0].value = "";
+          // $('#text_box')[0].value = JSON.stringify(data)
+        },
+        error: function(error) {
+          console.log('Error ${error}')
+        },
+      });
+
+    });
+});
